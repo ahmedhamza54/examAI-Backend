30ea0f0ddb9c235981a8dde5662e3eed8b0806a0
ahmedhamza54 <ahmed.hamza@esprit.tn>
2025-01-01 15:44:34 +0100

    azur port

--- src/main.ts
+++ src/main.ts
@@ -37,6 +37,7 @@ async function bootstrap() {
    const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document); // Swagger UI at /api
 
-  await app.listen(3000,'0.0.0.0');
+  const port = process.env.PORT || 3000; // Use Azure's port or default to 3000 for local development
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
